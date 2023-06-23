3b1c5507330a158b7eccee6cb83dfccbf4fee194
yusefHajeb <watanalihsas@gmail.com>
2023-06-23 03:54:44 +0300

    build data repository

---
+++ lib/data/valot/ability.dart
@@ -0,0 +1,13 @@
+class Ability {
+  Ability();
+
+  factory Ability.fromJson(Map<String, dynamic> json) {
+    // TODO: implement fromJson
+    throw UnimplementedError('Ability.fromJson($json) is not implemented');
+  }
+
+  Map<String, dynamic> toJson() {
+    // TODO: implement toJson
+    throw UnimplementedError();
+  }
+}
