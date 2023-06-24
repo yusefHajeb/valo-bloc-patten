98ddc85310695317d9422841139f5188e8bd0ccb
yusefHajeb <watanalihsas@gmail.com>
2023-06-25 01:15:35 +0300

    solve equatable proplem and hashCode by Fucnction

--- lib/data/model/charachter_model.dart
+++ lib/data/model/charachter_model.dart
@@ -1,9 +1,11 @@
 import 'package:valoyousefhajeb/data/model/ability_line.dart';
+import 'package:valoyousefhajeb/data/model/role_line.dart';
 import 'package:valoyousefhajeb/data/model/voice_line.dart';
 
 class CharachterModle {
   String? description, displayName, fullPortrait, displayIcon;
   List<Ability> abilities;
+  Role role;
   VoiceLine voiceLine;
   CharachterModle(
       {required this.description,
@@ -11,5 +13,6 @@ class CharachterModle {
       required this.fullPortrait,
       required this.abilities,
       required this.voiceLine,
+      required this.role,
       required this.displayName});
 }
